6e89ff2e0d6a581bb0e64062b42271b241611a57
Sheridan <sheridanmaustad@gmail.com>
2021-04-21 22:07:30 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+const { match } = require("sinon");
+function findMatching(arr, string){
+    console.log (arr);
+    console.log (string);
+    const matchName = arr.filter((name) => name.toLowerCase() === string.toLowerCase());
+    return matchName;
+}
+
+function fuzzyMatch(arr, string){
+    return arr.filter (function(match){
+        return match.toLowerCase().indexOf(string.toLowerCase()) === 0;
+    })
+}
+
+function matchName(arr, string){
+    return arr.filter( X => X.name === string);
+}
